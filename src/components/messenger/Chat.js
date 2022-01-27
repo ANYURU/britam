4baa1951a90812e9a570198f99aa4de7fc2c3e93
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2022-01-27 15:59:38 +0300

    Chat view partially working

--- src/components/messenger/Chat.js
+++ src/components/messenger/Chat.js
@@ -1,20 +1,32 @@
 import { useEffect, useState, useRef } from 'react';
 import {db, collection, authentication} from '../../helpers/firebase';
+
+import {Modal} from 'react-bootstrap'
+
 import SendMessage from './SendMessage';
+
 import {IoArrowBackOutline} from 'react-icons/io5'
 import {FaAngleDoubleDown, FaAngleDoubleUp} from 'react-icons/fa'
 import {BiEnvelope} from 'react-icons/bi'
+import {IoCloseSharp} from 'react-icons/io5'
+
+import useDialog from '../../hooks/useDialog'
 import './messenger.css'
 
 
 
+
 function Chat() {
+    const [ open, handleShow, handleClose] = useDialog()
+    const [ acceptedChats, setAcceptedChats ] = useState([{name:"Kizito Douglas"}, {name:"KiKibirango Sumayiya"}, {name:"Anyuru David Derrick"}, {name:"Charles Kasasira Derrick"}])
+
+
     const [ displayMessages, setDisplayMessages ] = useState(false)
     const [ messages, setMessages ] = useState([{text:'Hello', uid: "001", date:"Jan 24 2009", sendersUid:"oxsdUDhbTvMhdDbCRSb2Qaz0IAH3", receiversUid:"09398938"}, {text:'Hi', uid:"002", date:"Aug 23, 2021", sendersUid:"oxsdUDhbTvMhdDbCRSb2Qaz0IAH3", receiversUId:"09398938"},{text: "How're you doing? How was your day? Hope everything is okay. I would like to know you in person.", uid:"003", date:"Mar 21, 2022", sendersUid:"09398938", receiversUid:"oxsdUDhbTvMhdDbCRSb2Qaz0IAH3"}, {text:"I was supposed to come today but unfortunately. My puppy Machelle, fell sick and had to be rushed to the hospital. He started convulsing all of a sudden", date:"Jan 4, 2022", sendersUid:"oxsdUDhbTvMhdDbCRSb2Qaz0IAH3", receiversUid:"09398938"}])
     const [ user, setUser ] = useState(authentication.currentUser.displayName)
     const [ selectedChat, setSelectedChat ] = useState(false)
-    const [ selectChat, setSelectChat ] = useState(false)
-    const [ previousChats, setPreviouosChats ] = useState([{name:"Anyuru David Derrick", uid:"0920290", photoURL:"blablabla"}, {name:"Charles Kasasira Derrick", uid:"223848", photoURL:"tintintin"}  ])
+    const [ selectChat, setSelectChat ] = useState(true)
+    const [ previousChats, setPreviousChats ] = useState([{name:"Anyuru David Derrick", uid:"0920290", photoURL:"blablabla"}, {name:"Charles Kasasira Derrick", uid:"223848", photoURL:"tintintin"}  ])
     const [ expanded, setExpanded ] = useState(false)
     const scroll = useRef()
 
@@ -61,9 +73,7 @@ function Chat() {
                         Messages 
                     </div>
                     <div style={{display:"flex", gap:"5px"}}>
-                        <button onClick={() => {
-                            setSelectChat(!selectChat)
-                        }} style={{height:"30px", width:"30px", borderRadius:"50%", border:"none"}}>
+                        <button onClick={handleShow} style={{height:"30px", width:"30px", borderRadius:"50%", border:"none"}}>
                             <i style={{height:"100%", width:"100%", display:"flex", justifyContent:"center", alignItems:"center", borderRadius:"50%", backgroundColor:"#f7f9f9"}}><BiEnvelope /></i>
                         </button>
                         <button style={{height:"30px", width:"30px", borderRadius:"50%", border:"none", marginRight:"20px"}} onClick={()=>{
@@ -76,9 +86,52 @@ function Chat() {
                                 document.getElementById("msg-form").classList.add("collapse-form")
                                 setExpanded(!expanded)
                             }
+
+                            
+
+
+
                         }}>
                             <i style={{height:"100%", width:"100%", display:"flex", justifyContent:"center", alignItems:"center", borderRadius:"50%", backgroundColor:"#f7f9f9"}}>{expanded === true ? <FaAngleDoubleDown /> : <FaAngleDoubleUp />}</i>
                         </button>
+
+                        <Modal show={open} onHide={handleClose} className="sm-shadow" style={{border:"none"}}>
+                                <Modal.Header>
+                                    <Modal.Title>
+                                        <div style={{display:"flex", gap:"5px"}}> 
+                                            <button onClick={handleClose} style={{height:"30px", width:"30px", borderRadius:"50%", border:"none"}}>
+                                                <i style={{height:"100%", width:"100%", display:"flex", justifyContent:"center", alignItems:"center", borderRadius:"50%", backgroundColor:"#f7f9f9", fontSize:"17px"}}><IoCloseSharp /></i>
+                                            </button>   
+                                            <h6 style={{fontSize:"18px", opacity:"0.9", paddingTop:"5px"}}>
+                                                New Message
+                                            </h6>
+                                        </div>
+                                    </Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body style={{borderTop:"solid 0.2px #cfd9de", overflow:"scroll", scrollBehavior:"smooth"}}>
+                                {
+                                    previousChats.map(({
+                                        name,
+                                        photoURL
+                                    }, index) => {
+                                        return (
+                                            <div style={{display:"flex", gap:"5px", alignItems:"center", cursor:"pointer"}} onClick={(event) => {
+                                                event.preventDefault()
+                                                setPreviousChats(...previousChats, {name, photoURL})
+
+                                            }}>
+                                                <div>
+                                                    <div style={{width:"30px",  height:"30px", borderRadius:"50%", backgroundColor:"gray", opacity:"0.2", display:"flex", justifyContent:"center", alignItems:"center"}}><div>{`${name.split(" ")[0][0].toUpperCase()}${name.split(" ")[1][0].toUpperCase()}`}</div></div>
+                                                </div>
+                                                
+                                                <p style={{paddingTop:"6px", height:"100%"}}>{name}</p>
+                                                
+                                            </div>
+                                        );
+                                    }) 
+                                }
+                                </Modal.Body>
+                        </Modal>
                     </div>
                 </div>
                 
@@ -86,21 +139,25 @@ function Chat() {
                 
             <div style={{height:"400px", width:"300px", backgroundColor:"white", borderTopLeftRadius:"15px 15px", borderTopRightRadius:"15px 15px", paddingLeft:"20px", overflow:"scroll", scrollBehavior:"smooth"}}>
                 {
-                    selectChat === true &&
+                    selectChat === true ?
                     previousChats.map(({
                         name,
                         photoURL
                     }, index) => {
                         return (
-                            <div key={index} style={{marginTop: "20px"}}>
-                                {name}
+                            <div style={{display:"flex", gap:"5px", alignItems:"center", cursor:"pointer"}} onClick={() => {
+                                setSelectChat(!selectChat)
+                            }}>
+                                <div>
+                                    <div style={{width:"40px",  height:"40px", borderRadius:"50%", backgroundColor:"gray", opacity:"0.2", display:"flex", justifyContent:"center", alignItems:"center"}}><div>{`${name.split(" ")[0][0].toUpperCase()}${name.split(" ")[1][0].toUpperCase()}`}</div></div>
+                                </div>
+                                <div key={index} style={{marginTop: "25px", display:"flex", alignItems:"center", height:"100%"}}>
+                                    <p>{name}</p>
+                                </div>
                             </div>
                         );
-
                     }) 
-                }
-                {
-                    selectedChat === false &&
+                    :
                     messages.map(({text, date, sendersUid}, index) => {
                         if(sendersUid === authentication.currentUser.uid) {
                             // console.log(text)
@@ -122,7 +179,7 @@ function Chat() {
                                 </div>
                             );
                         } 
-
+                        
                         return (
                             <div key={index} style={{marginTop:"20px" }}>
                                 <div className="msg-container" style={{display:"flex", justifyContent:"flex-end", paddingRight:"20px"}}>
@@ -139,8 +196,9 @@ function Chat() {
                         );
                     })
                 }
+                    
             </div>
-            <div id="msg-form" style={{width:"100%", backgroundColor:"white", borderTop:"solid 1px #eff3f4", height:"50px"}} className="collapse-form">
+            <div id="msg-form" style={{width:"100%", backgroundColor:"white", borderTop:"solid 1px #eff3f4", height:"50px"}}>
                 <SendMessage scroll={scroll}/>
             </div>
             <div ref={scroll}></div>
